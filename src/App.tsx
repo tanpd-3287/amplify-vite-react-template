3ae6e8636938d9e75f77072659b841023bc38b5b
tanpd-3287 <phan.dinh.tan@sun-asterisk.com>
2024-11-17 21:56:13 +0700

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -17,13 +19,20 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
+      <button onClick={signOut}>Sign out</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+        {todos.map((todo) => (<li           
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
